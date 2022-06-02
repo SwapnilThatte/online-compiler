ebde3c2bb68aa6ce044d2c6f35e471f3e46a7cc2
SwapnilThatte <90109469+SwapnilThatte@users.noreply.github.com>
2022-06-02 21:25:50 +0530

    Update executeJS.js

--- executeJS.js
+++ executeJS.js
@@ -9,18 +9,13 @@ if (!fs.existsSync(outputPath)) {
 }
 
 const executeJS = (filepath) => {
- // console.log(filepath);
   const jobId = path.basename(filepath).split(".")[0];
-  //const outPath = path.join(outputPath, `${jobId}.py`);
-  //console.log(outPath);
 
- //console.log(__dirname);
   return new Promise((resolve, reject) => {
     exec(
       `cd ${__dirname}/code & node ${jobId}.js` ,
       (error, stdout, stderr) => {
         error && reject({'error': error, 'stderr': stderr });
-        //console.log(`ERR: ${error}\nSTDERR: ${stderr}`);
         stderr && reject(stderr);
         resolve({'stdout':stdout, 'error':error})
       }
